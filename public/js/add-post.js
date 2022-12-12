871ad4d62b07c712d28648299405db1a290158a0
404pandas <113853550+404pandas@users.noreply.github.com>
2022-12-12 10:48:15 -0500

    Added javascript for all functionality and basic css styling

---
+++ public/js/add-post.js
@@ -0,0 +1,27 @@
+// Form handler, query selector, and event listener for adding posts
+
+async function newFormHandler(event) {
+    event.preventDefault();
+
+    const title = document.querySelector('input[name="post-title"]').value;
+    const post_text = document.querySelector('textarea[name="post-text"]').value;
+
+    const response = await fetch('/api/posts', {
+        method: 'POST',
+        body: JSON.stringify({
+            title,
+            post_text
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    if (response.ok) {
+        document.location.replace('/dashboard');
+    } else {
+        alert(response.statusText);
+    }
+}
+
+document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
---
+++ public/js/logout.js
@@ -0,0 +1,16 @@
+// Form handler, query selector, and event listener for logging out
+
+async function logout() {
+    const response = await fetch('/api/users/logout', {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' }
+    });
+  
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert(response.statusText);
+    }
+  }
+  
+  document.querySelector('#logout').addEventListener('click', logout);
